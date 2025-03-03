427eab4d28a0fd708da709360a5f4e379fdf757f
Levi Wallace <leviwallace2002@gmail.com>
2025-03-02 17:09:27 -0800

    (+) auth: protected route

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { fetchAuthSession } from "aws-amplify/auth";
 
 import IndexPage from "@/pages/index";
 import AuthenticationPage from "@/pages/auth";
@@ -6,6 +8,29 @@ import LandingPage from "@/pages/landing";
 // import VehiclesPage from "@/pages/landing/vehicles";
 import TracksPage from "./pages/landing/tracks";
 
+
+
+function ProtectedRoute({ children }: { children: React.ReactNode }) {
+  const [authenticated, setAuthenticated] = useState<boolean | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      const result = await fetchAuthSession();
+      if (result.tokens)
+        setAuthenticated(true);
+      else {
+        setAuthenticated(false);
+      }
+      setIsLoading(false);
+    };
+
+    checkAuth();
+  }, []);
+  
+  if (isLoading) return null;
+  return authenticated ? <>{children}</> : <Navigate to="/auth" />;
+};
 /**
  * The main application component that sets up the routing for the application.
  * 
@@ -22,10 +47,11 @@ function App() {
     <Routes>
       <Route element={<IndexPage />} path="/" />
       <Route element={<AuthenticationPage />} path="/auth" />
-      <Route element={<LandingPage />} path="/landing" />
+
+      {/* Landing Page */}
+      <Route  element={<ProtectedRoute> <LandingPage /> </ProtectedRoute>} path="/landing" />
 
       {/* Personal Account */}
-      {/* <Route element={<VehiclesPage />} path="/landing/vehicles" /> */}
       <Route element={<TracksPage />} path="/landing/tracks" />
 
     </Routes>
