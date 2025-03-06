def69b78952fe2f2d6e83ba7051a8eead7bc6d21
Levi Wallace <leviwallace2002@gmail.com>
2025-03-06 10:42:54 -0800

    (+) landing: pointers

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -24,16 +24,16 @@ export default function LandingPage() {
         <>
             <Navbar />
             <div className="flex flex-row h-screen w-full absolute top-0">
-                <div className="flex w-1/3 h-screen hover:w-1/2 transition-all duration-500 bg-background items-center justify-center relative overflow-hidden">
+                <div className="flex w-1/3 h-screen hover:w-1/2 transition-all duration-500 bg-background items-center justify-center relative overflow-hidden cursor-pointer" onClick={() => navigate('/landing/tracks')}>
                     <video className="h-full w-full object-cover absolute top-0 left-0 opacity-20 hover:opacity-50 transition-opacity" src={tracks} loop muted autoPlay></video>
-                    <div className="cursor-pointer relative z-10" onClick={() => navigate('/landing/tracks')}>
+                    <div className="pointer-events-none relative z-10">
                         <h1 className="text-foreground  text-center font-play text-5xl font-bold tracking-tighter">Tracks</h1>
                         <h4 className="text-foreground text-center font-play text-2xl font-thin tracking-tighter">Enter Tracks</h4>
                     </div>
                 </div>
-                <div className="flex w-1/3 h-screen hover:w-1/2 transition-all duration-500  bg-background items-center justify-center relative overflow-hidden">
+                <div className="flex w-1/3 h-screen hover:w-1/2 transition-all duration-500  bg-background items-center justify-center relative overflow-hidden cursor-pointer" onClick={() => navigate('/landing/sweeps')}>
                     <video className="h-full w-full object-cover absolute top-0 left-0 opacity-20 hover:opacity-50 transition-opacity" src={sweeps} loop muted autoPlay></video>
-                    <div className="cursor-pointer relative z-10" onClick={() => navigate('/landing/sweeps')}>
+                    <div className="pointer-events-none relative z-10">
                         <h1 className="text-foreground text-center font-play text-5xl font-bold tracking-tighter">Sweeps</h1>
                         <h4 className="text-foreground text-center font-play text-2xl font-thin tracking-tighter">Enter Sweeps</h4>
                     </div>
@@ -41,19 +41,19 @@ export default function LandingPage() {
                 <div className="flex w-1/3 h-screen hover:w-1/2 transition-all duration-50 items-center justify-center relative overflow-hidden">
                     <video className="h-full w-full object-cover absolute top-0 left-0" src={vehicles} loop muted autoPlay></video>
                     <div className="flex flex-col w-full h-full">
-                        <div className="flex h-1/2 hover:h-full w-full transition-all duration-200 relative">
-                            <div className="h-full w-full bg-background opacity-80 hover:opacity-20 border-b-1 z-10 border-black absolute transition-opacity duration-500"></div>
+                        <div className="flex h-1/2 hover:h-full w-full transition-all duration-200 relative cursor-pointer" onClick={() => navigate('/landing/vehicles')}>
+                            <div className="h-full w-full bg-background opacity-80 hover:opacity-50 border-b-1 z-10 border-black absolute transition-opacity duration-500"></div>
                             <div className="flex h-full w-full items-center justify-center absolute">
-                                <div className="cursor-pointer relative z-10" onClick={() => navigate('/landing/vehicles')}>
+                                <div className="pointer-events-none relative z-10">
                                     <h1 className="text-foreground text-center font-play text-5xl font-bold tracking-tighter">Vehicles</h1>
                                     <h4 className="text-foreground text-center font-play text-2xl font-thin tracking-tighter">Enter Vehicles</h4>
                                 </div>
                             </div>
                         </div>
-                        <div className="flex h-1/2 hover:h-full w-full transition-all duration-200 relative">
-                            <div className="h-full w-full bg-background opacity-80 hover:opacity-20 border-b-1 z-10 border-black absolute transition-opacity duration-500"></div>
+                        <div className="flex h-1/2 hover:h-full w-full transition-all duration-200 relative cursor-pointer" onClick={() => navigate('/landing/components')}>
+                            <div className="h-full w-full bg-background opacity-80 hover:opacity-50 border-b-1 z-10 border-black absolute transition-opacity duration-500"></div>
                             <div className="flex h-full w-full items-center justify-center absolute">
-                                <div className="cursor-pointer relative z-10" onClick={() => navigate('/landing/components')}>
+                                <div className="pointer-events-none relative z-10">
                                     <h1 className="text-foreground text-center font-play text-5xl font-bold tracking-tighter">Vehicle Components</h1>
                                     <h4 className="text-foreground text-center font-play text-2xl font-thin tracking-tighter">Enter Vehicle Components</h4>
                                 </div>
