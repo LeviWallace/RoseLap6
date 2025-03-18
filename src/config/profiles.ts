c62739eb85345e32d7e2ae0de53210a22a6d0430
rhit-jinrc <112568279+rhit-jinrc@users.noreply.github.com>
2025-03-18 19:32:20 -0400

    profileConfig with team members

---
+++ src/config/profiles.ts
@@ -0,0 +1,64 @@
+export type Profile = typeof profileConfig;
+
+export const profileConfig = [
+    {
+        picture: "../src/assets/historypage/profiles/ryan.jpg",
+        name: "Ryan Jin",
+        role: "Team Manager",
+    },
+    {
+        picture: "../src/assets/historypage/profiles/victor.jpg",
+        name: "Victor Zhang",
+        role: "Technical Director, Chassis Lead",
+    },
+    {
+        picture: "../src/assets/historypage/profiles/josh.jpg",
+        name: "Josh Brunner",
+        role: "Trasurer",
+    },
+    {
+        picture: "../src/assets/historypage/profiles/jimmy.jpg",
+        name: "Jimmy Zhang",
+        role: "Sponsorship Lead",
+    },
+    {
+        picture: "../src/assets/historypage/profiles/axel.jpg",
+        name: "Axel Johnson",
+        role: "Unsprung, Data & Sim Lead",
+    },
+    {
+        picture: "../src/assets/historypage/profiles/eric.jpg",
+        name: "Eric Soo-Yshimura",
+        role: "Electrical Lead",
+    },
+    {
+        picture: "../src/assets/historypage/profiles/taha.jpg",
+        name: "Taha Benmoussa",
+        role: "Powertrain Lead",
+    },
+    {
+        picture: "../src/assets/historypage/profiles/will.jpg",
+        name: "Will Hartman",
+        role: "Drivetrain Lead",
+    },
+    {
+        picture: "../src/assets/historypage/profiles/sunil.jpg",
+        name: "Sunil Nadharajan",
+        role: "Brakes Lead",
+    },
+    {
+        picture: "../src/assets/historypage/profiles/evan.jpg",
+        name: "Evan Eades",
+        role: "Aerodynamics Lead",
+    },
+    {
+        picture: "../src/assets/historypage/profiles/thor.jpg",
+        name: "Thor Thomas",
+        role: "Suspension Lead",
+    },
+    {
+        picture: "../src/assets/historypage/profiles/jared.jpg",
+        name: "Jared Doeden",
+        role: "Ergonomics Lead",
+    }
+]
--- src/pages/history.tsx
+++ src/pages/history.tsx
@@ -1,50 +1,8 @@
 import OverlayLayout from "@/layouts/overlay";
 import Profile from "@/components/profile";
+import { profileConfig } from "@/config/profiles";
 
 export default function HistoryPage() {
-  const profiles = [
-    {
-      picture: "../src/assets/historypage/profiles/axel.jpg",
-      name: "Axel Johnson",
-      role: "Unsprung, Data & Sim Lead",
-    },
-    {
-      picture: "../src/assets/historypage/profiles/axel.jpg",
-      name: "Axel Johnson",
-      role: "Unsprung, Data & Sim Lead",
-    },
-    {
-      picture: "../src/assets/historypage/profiles/axel.jpg",
-      name: "Axel Johnson",
-      role: "Unsprung, Data & Sim Lead",
-    },
-    {
-      picture: "../src/assets/historypage/profiles/axel.jpg",
-      name: "Axel Johnson",
-      role: "Unsprung, Data & Sim Lead",
-    },
-    {
-      picture: "../src/assets/historypage/profiles/axel.jpg",
-      name: "Axel Johnson",
-      role: "Unsprung, Data & Sim Lead",
-    },
-    {
-      picture: "../src/assets/historypage/profiles/axel.jpg",
-      name: "Axel Johnson",
-      role: "Unsprung, Data & Sim Lead",
-    },
-    {
-      picture: "../src/assets/historypage/profiles/axel.jpg",
-      name: "Axel Johnson",
-      role: "Unsprung, Data & Sim Lead",
-    },
-    {
-      picture: "../src/assets/historypage/profiles/axel.jpg",
-      name: "Axel Johnson",
-      role: "Unsprung, Data & Sim Lead",
-    },
-  ];
-
   return (
     <OverlayLayout
       source="../src/assets/historypage/team2024.jpg"
@@ -104,7 +62,7 @@ export default function HistoryPage() {
       {/* Profiles */}
       <div className="w-full flex justify-center mt-10">
         <div className="grid grid-cols-4 gap-8 max-w-8xl">
-          {profiles.map((profile, index) => (
+          {profileConfig.map((profile, index) => (
             <Profile
               key={index}
               picture={profile.picture}
